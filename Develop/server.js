0710a544e4d17a385c0bee210ba4f37922f1a441
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-11-07 13:40:01 -0800

    server.js file complete

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({helpers});
 
 const sess = {
   secret: 'Super secret secret',
@@ -18,11 +19,13 @@ const sess = {
   })
 };
 
+app.engine("handlebars",hbs.engine);
+app.set("view engine", "handlebars");
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
